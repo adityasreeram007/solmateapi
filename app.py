0df1e3af8babdd3dbb1754226373fe7673bacd2c
aditya <adityasreeram99@gmail.com>
2020-06-19 19:13:03 +0530

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,jsonify
+import csv
+app=Flask(__name__)
+
+vals=[]
+with open('questions.csv','r') as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        vals.append(row)
+print(vals)
+
+@app.route('/',methods=['GET'])      
+def senddata():
+    
+    return jsonify({'data':vals})
+    
+if __name__=='__main__':
+    app.run(debug=True)
